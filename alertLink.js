e5e82821bd80f827b2b5a0d1c98b404d9c28437a
gautamsuraj <suraj.g@sevencollab.com>
2022-03-19 19:02:50 +0545

    fix: complete functional requirements

---
+++ alertLink.js
@@ -0,0 +1,21 @@
+document.addEventListener('click', e => {
+    getStorageValue("enabled").then(result => {
+        if (result) {
+            var anchorElement = e.target.closest('a');
+            if (anchorElement && anchorElement.href) {
+                // e.preventDefault();
+                alert('Do you trust this link? ' + anchorElement.href)
+                window.location.href = anchorElement.href;
+            }
+        }
+    });
+})
+
+const getStorageValue = async (key) => {
+    let p = new Promise(((resolve, reject) => {
+        chrome.storage.sync.get(key, (result) => {
+            resolve(result[key]);
+        });
+    }))
+    return await p;
+}
