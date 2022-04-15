b26e50866029d3cfdb162cd000b7ccf633b3d828
gautamsuraj <suraj.g@sevencollab.com>
2022-04-15 19:37:37 +0545

    fix: complete

---
+++ blocklink.js
@@ -0,0 +1,38 @@
+// document.addEventListener('click', e => {
+//     getStorageValue("enabled").then(result => {
+//         if (result) {
+//             var anchorElement = e.target.closest('a');
+//             if (anchorElement && anchorElement.href) {
+//                 alert('prevented')
+//                 e.preventDefault();
+//                 e.stopPropagation();
+//                 e.stopImmediatePropagation();
+//                 return false;
+//             }
+//         }
+//     });
+// })
+
+document.addEventListener('click',  e => {
+    //preventing by default at first because e.preventDefault() doesn't work after async call.
+    e.preventDefault();
+    getStorageValue("enabled").then(result => {
+        var anchorElement = e.target.closest('a');
+        if (!result) {
+            //if not enabled, letting user click links by setting window.location
+            if (anchorElement && anchorElement.href) {
+                window.location = anchorElement.href;
+            }
+        }
+    });
+})
+
+async function getStorageValue(key) {
+    let p = new Promise(((resolve, reject) => {
+        chrome.storage.sync.get(key, (result) => {
+            resolve(result[key]);
+        });
+    }))
+    return await p;
+}
+
